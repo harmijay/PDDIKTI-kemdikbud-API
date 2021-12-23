573de6af6ee17b8b27820ef0c6216f47ae055a5e
Rizki Afrizal <srizkiafrizal85@gmail.com>
2021-12-23 09:31:24 +0700

    revise scrape.py
    adding try catch so no error anymore

--- scrape.py
+++ scrape.py
@@ -1,21 +1,31 @@
 from pddiktipy import api
+import time
 
 a = api()
 
 list_all_univ = a.dump_all_univ()
 
-f = open("list_email.txt", "a")
 list_all_univ_name = []
+list_to_write_to_f = []
+list_to_write_to_f_2 = []
 for univ in list_all_univ:
     nama_pt = univ['nama_pt']
-    pt = a.detail_data_univ_by_name(nama_pt)
-    if "email" in pt and pt["email"] is not None and pt["email"] != "" and pt["email"] != " ":
-        email_pt = pt['email']
-    else :
-        email_pt = 'none'
-    f.write(email_pt + " ---- " +nama_pt +"\n")
-f.close()
+    try:  
+        pt = a.detail_data_univ_by_name(nama_pt)  
+        if "email" in pt and pt["email"] is not None and pt["email"] != "" and pt["email"] != " ":
+            email_pt = pt['email']
+        else :
+            email_pt = 'none'
+        list_to_write_to_f.append(pt["id_sp"] + " ---- " + email_pt + " ---- " + nama_pt +"\n")
+    except:
+        list_to_write_to_f_2.append(nama_pt +"\n")
 
+f = open("list_email.txt", "a")
+f.write("\n".join(list_to_write_to_f))
+f.close()
+f_2 = open("list_kampus_error.txt","a")
+f_2.write("\n".join(list_to_write_to_f_2))
+f_2.close()
 
 
 # list_pt_hash = []
