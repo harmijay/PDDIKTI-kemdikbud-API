e6d980054db7e1487faae749d0b641ad9894b70e
Rizki Afrizal <srizkiafrizal85@gmail.com>
2022-12-23 13:33:27 +0700

    last change

--- scrape.py
+++ scrape.py
@@ -3,29 +3,37 @@ import time
 
 a = api()
 
-list_all_univ = a.dump_all_univ()
+print(a.detail_data_univ_by_name("King Saud University"))
 
-list_all_univ_name = []
-list_to_write_to_f = []
-list_to_write_to_f_2 = []
-for univ in list_all_univ:
-    nama_pt = univ['nama_pt']
-    try:  
-        pt = a.detail_data_univ_by_name(nama_pt)  
-        if "email" in pt and pt["email"] is not None and pt["email"] != "" and pt["email"] != " ":
-            email_pt = pt['email']
-        else :
-            email_pt = 'none'
-        list_to_write_to_f.append(pt["id_sp"] + " ---- " + email_pt + " ---- " + nama_pt +"\n")
-    except:
-        list_to_write_to_f_2.append(nama_pt +"\n")
+# list_all_univ = a.dump_all_univ()
 
-f = open("list_email.txt", "a")
-f.write("\n".join(list_to_write_to_f))
-f.close()
-f_2 = open("list_kampus_error.txt","a")
-f_2.write("\n".join(list_to_write_to_f_2))
-f_2.close()
+# f = open("list_email.txt", "a")
+# f_2 = open("list_kampus_error.txt","a")
+
+# list_all_univ_name = []
+# list_to_write_to_f = []
+# list_to_write_to_f_2 = []
+# for univ in list_all_univ:
+#     nama_pt = univ['nama_pt']
+#     try:  
+#         pt = a.detail_data_univ_by_name(nama_pt)  
+#         if "email" in pt and pt["email"] is not None and pt["email"] != "" and pt["email"] != " ":
+#             email_pt = pt['email']
+#         else :
+#             email_pt = 'none'
+#         # list_to_write_to_f.append(pt["id_sp"] + " ---- " + email_pt + " ---- " + nama_pt +"\n")
+#         f.write(pt["id_sp"] + " ---- " + email_pt + " ---- " + nama_pt +"\n")
+#     except:
+#         # list_to_write_to_f_2.append(nama_pt +"\n")
+#         print("EXCEPT "+nama_pt)
+#         f_2.write(nama_pt +"\n")
+
+# # f = open("list_email.txt", "a")
+# # f.write("\n".join(list_to_write_to_f))
+# f.close()
+# # f_2 = open("list_kampus_error.txt","a")
+# # f_2.write("\n".join(list_to_write_to_f_2))
+# f_2.close()
 
 
 # list_pt_hash = []
